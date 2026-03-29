35a69f1fccf4d39fe6a108336aa31b17c26e1d64
tacal100 <t.caliskan@live.de>
2026-03-29 15:57:41 +0200

    add authentik users types and fix redirection

---
+++ dataprocessing/dataprocessing.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+import authentik_client
+from authentik_client.exceptions import ApiException
+from authentik_client.models.user import User
+from fastapi import HTTPException
+
+AUTHENTIK_URL = "https://auth.saw.rwth-aachen.de/api/v3"
+
+def getProcessedUsers(token: str, attributes: Optional[str]) -> list[User]: 
+    if not token:
+        raise HTTPException(status_code=401, detail="Missing access token")
+
+    configuration = authentik_client.Configuration(
+        host=AUTHENTIK_URL,
+        access_token=token,
+    )
+
+    all_users = []
+    page = 1
+
+    with authentik_client.ApiClient(configuration) as api_client:
+        api = authentik_client.CoreApi(api_client)
+
+        ## Iterate over all available pages of users (authentik maxes out at 100 users page_size)
+        while True:
+            try:
+                kwargs = {"page": page, "page_size": 500, 'attributes': ''}
+                if attributes:
+                    kwargs["attributes"] = attributes
+
+                response = api.core_users_list(**kwargs)
+
+                all_users.extend([user for user in response.results])
+
+                if page >= response.pagination.total_pages:
+                    break
+                page += 1
+
+            except ApiException as e:
+                print(f"Request failed: {e}")
+                break
+
+    return all_users
--- main.py
+++ main.py
@@ -1,50 +1,56 @@
+import base64
 import json
 from typing import Optional
+from urllib.parse import urlparse
 
+from authentik_client import User
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
 
 from auth import getOAuth, getSessionSecret
-from dataprocessing.user_dataclasses import User
+from dataprocessing.dataprocessing import getProcessedUsers
 
 app = FastAPI()
-AUTHENTIK_URL = "auth.saw.rwth-aachen.de"
 
 # Der secret key hier ist um den session cookie mit dem frontend zu verifizieren. 
-app.add_middleware(SessionMiddleware, secret_key=getSessionSecret())
-
-@app.get("/")
-def home(request: Request):
-    return RedirectResponse(url=request.url_for("login"))
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=getSessionSecret(),
+    same_site="lax",
+    https_only=False,  # True in production
+)
 
 @app.get("/auth/login", description="Redirects the user to the login form for authentik (if not signed in already)")
-async def login(request: Request):
-    if request.session.get("user"):
-        return RedirectResponse(url=request.url_for("getUsers"));
-    
-    redirect_uri = str(request.url_for("callback"))
-    return await getOAuth().authentik.authorize_redirect(request, redirect_uri)
+async def login(request: Request, current_url: str):
+    request.session.clear() 
+    request.session["return_to"] = current_url
+    ## Parse url because it comes with weird characters for certain symbols
+    parsed = urlparse(current_url)
+    frontend_origin = f"{parsed.scheme}://{parsed.netloc}"
+    redirect_uri = f"{frontend_origin}/api/auth/callback"
+    state = base64.urlsafe_b64encode(json.dumps({"return_to": current_url}).encode()).decode()                                                                             
+    return await getOAuth().authentik.authorize_redirect(request, redirect_uri, state=state)
 
-# Stattdessen sollte der user wieder auf der URL landen über die er zum login redirected wurde.
 @app.get("/auth/callback", description="Stores session token in server-side session. Should redirect to original page")
 async def callback(request: Request):
     token = await getOAuth().authentik.authorize_access_token(request)
-    request.session["access_token"] = token["access_token"]
-    return RedirectResponse(url=request.url_for("getUsers"))
+    state = json.loads(base64.urlsafe_b64decode(request.query_params["state"]))                                                                                            
+    request.session["access_token"] = token["access_token"]                                                                                                                
+    return RedirectResponse(url=state["return_to"])     
 
-@app.get("/users")
-async def getUsers(request: Request, attributes: Optional[str] = ''):   
+@app.get("/users", description="Returns a list of users (todo: based on the current users view permissions)", response_model= list[User], response_model_exclude={"avatar", "uid", "uuid", "type", "path", "groups", "roles"})
+async def getUsers(request: Request , attributes: Optional[str] = ''):   
+    token = requireToken(request)
+    users = getProcessedUsers(token, attributes)
+    return users
+
+
+def requireToken(request: Request):
     token = request.session.get("access_token")
     if not token:
-        raise HTTPException(status_code=401, detail="Missing access token")
-        
-    params = {"attributes": attributes} if attributes else {}
-
-    resp = await getOAuth().authentik.get(
-        f"https://{AUTHENTIK_URL}/api/v3/core/users/",
-        params=params,
-        token={"access_token": token, "token_type": "Bearer"}
-    )
-
-    return resp.json()["results"]
+        raise HTTPException(
+            status_code=401,
+            detail={"message": "Not authenticated"}
+        )
+    return str(token)
